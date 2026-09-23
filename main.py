3ec401a5eb75af4a4710c8c39e7a4a1f98faf75b
Konstantin Sherbakov <__GitHub>
2026-09-23 21:03:35 +0300

    Show URL in status output

--- main.py
+++ main.py
@@ -2,5 +2,6 @@ import requests
 
 print("Hello, World!")
 
-r = requests.get("https://google.com", timeout=10)
-print(r.status_code)
+url = "https://google.com"
+r = requests.get(url, timeout=10)
+print(f"Status code from {url}: {r.status_code}")
